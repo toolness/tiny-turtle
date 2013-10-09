236ea4d70fce782f4ca2e97250f022e431a627e9
Atul Varma <varmaa@gmail.com>
2013-10-09 08:24:18 -0400

    prevent flicker.

--- index.worker.js
+++ index.worker.js
@@ -1,10 +1,11 @@
 importScripts('tiny-turtle.js');
 
 TinyTurtle.drawLine = function(canvas, style, width, x1, y1, x2, y2) {
-  postMessage([style, width, x1, y1, x2, y2]);
+  postMessage({msg: 'line', args: [style, width, x1, y1, x2, y2]});
 };
 
 onmessage = function(e) {
   TinyTurtle.call(self, {width: e.data.width, height: e.data.height});
   eval(e.data.source);
+  postMessage({msg: 'done'});
 };
