af3697239607562d2e183f4cd32408635cd344a0
Atul Varma <varmaa@gmail.com>
2013-10-08 23:28:56 -0400

    code in index.html can now be edited and tinkered with.

---
+++ index.worker.js
@@ -0,0 +1,27 @@
+importScripts('tiny-turtle.js');
+
+function FakeCanvas(width, height) {
+  this.width = width;
+  this.height = height;
+}
+
+FakeCanvas.prototype = {
+  getContext: function() { return this; },
+  beginPath: function() {},
+  moveTo: function(x, y) { this._start = {x: x, y: y}; },
+  lineTo: function(x, y) { this._end = {x: x, y: y}; },
+  stroke: function() {
+    postMessage({
+      cmd: 'line',
+      start: this._start,
+      end: this._end,
+      strokeStyle: this.strokeStyle,
+      lineWidth: this.lineWidth
+    });
+  }
+};
+
+onmessage = function(e) {
+  TinyTurtle.call(self, new FakeCanvas(e.data.width, e.data.height));
+  eval(e.data.source);
+};
